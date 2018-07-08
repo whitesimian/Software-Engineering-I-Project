c5ba29ee258d4af60dbae976706c45a235b5f450
Felipe César L. Machado <38870713+whitesimian@users.noreply.github.com>
2018-07-08 18:56:19 -0300

    Formatted

--- Model.h
+++ Model.h
@@ -1,5 +1,5 @@
 /*
-* FELIPE CÉSAR LOPES MACHADO - 16.2.5890
+* FELIPE CÃ‰SAR LOPES MACHADO - 16.2.5890
 * UFOP - UNIVERSIDADE FEDERAL DE OURO PRETO
 * JUL/2018
 */
@@ -21,15 +21,15 @@ class Model{
 public:
 	static Model* new_model(int time = 0);
 
-	virtual vector<Flow *>::iterator flowEnd()			= 0;
+	virtual vector<Flow *>::iterator flowEnd()		= 0;
 	virtual vector<Flow *>::iterator flowBegin()		= 0;
 	virtual vector<System *>::iterator systemEnd()		= 0;
 	virtual vector<System *>::iterator systemBegin()	= 0;
 	
-	virtual size_t flow_amount()						= 0;
-	virtual size_t system_amount()						= 0;
-	virtual bool flow_resize_one_more()					= 0;
-	virtual bool system_resize_one_more()				= 0;
+	virtual size_t flow_amount()				= 0;
+	virtual size_t system_amount()				= 0;
+	virtual bool flow_resize_one_more()			= 0;
+	virtual bool system_resize_one_more()			= 0;
 
 	static System* add_system(const std::string&, int);
 
@@ -40,13 +40,13 @@ public:
 	static Flow* add_flow(const string&, const string&, const string&);
 
 	static Model* get_instance();
-	virtual bool erase_system(const string&)			= 0;
-	virtual bool erase_flow(const string&)				= 0;
-	virtual bool set_time(int)							= 0;
-	virtual int get_cur_time()							= 0;
+	virtual bool erase_system(const string&)		= 0;
+	virtual bool erase_flow(const string&)			= 0;
+	virtual bool set_time(int)				= 0;
+	virtual int get_cur_time()				= 0;
 	virtual System* system_exists(const string&)		= 0;
-	virtual Flow* flow_exists(const string&)			= 0;
-	virtual bool clear()								= 0;
+	virtual Flow* flow_exists(const string&)		= 0;
+	virtual bool clear()					= 0;
 
-	virtual bool run(int)								= 0;
+	virtual bool run(int)					= 0;
 };
