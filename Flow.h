5ba76cf685a1f211fcd7e0018dd650d179fc46ea
Felipe César L. Machado <38870713+whitesimian@users.noreply.github.com>
2018-07-08 18:57:55 -0300

    Formatted

--- Flow.h
+++ Flow.h
@@ -1,5 +1,5 @@
 /*
-* FELIPE CÉSAR LOPES MACHADO - 16.2.5890
+* FELIPE CÃ‰SAR LOPES MACHADO - 16.2.5890
 * UFOP - UNIVERSIDADE FEDERAL DE OURO PRETO
 * JUL/2018
 */
@@ -17,15 +17,15 @@ public:
 	virtual ~Flow() {};
 
 	virtual std::string get_name() const		= 0;
-	virtual System* get_source()				= 0;
-	virtual System* get_target()				= 0;
+	virtual System* get_source()			= 0;
+	virtual System* get_target()			= 0;
 	virtual bool set_name(const std::string&)	= 0;
 	virtual bool set_source_system(System *)	= 0;
 	virtual bool set_target_system(System *)	= 0;
 
-	virtual Flow& operator=(Flow *)				= 0;
-	virtual bool operator==(Flow *)				= 0;
-	virtual bool operator!=(Flow *)				= 0;
+	virtual Flow& operator=(Flow *)			= 0;
+	virtual bool operator==(Flow *)			= 0;
+	virtual bool operator!=(Flow *)			= 0;
 
-	virtual double flow_funct()					= 0;
+	virtual double flow_funct()			= 0;
 };
